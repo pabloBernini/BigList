669ec28841b8c23f31c13da007df90b9cda6f121
pabloBernini <chwast3712@gmail.com>
2025-05-15 23:59:01 +0200

    Added readme and loading indicator

---
+++ README.md
@@ -0,0 +1,22 @@
+# BigListApp
+
+A simple Android application that displays a list of users and their associated posts and todos from the JSONPlaceholder API.
+
+
+### User List Display
+
+This GIF shows how the application displays a scrollable list of users:
+
+![User List Display](gifs/gif4.gif)
+
+### Post Details Screen
+
+This GIF demonstrates navigating to a user's posts and viewing the details of a selected post:
+
+![Post Details Screen](gifs/gif2.gif)
+
+### User Details Screen with Todos
+
+This GIF shows the user details screen, including the user's information and a list of their todos:
+
+![User Details Screen with Todos](gifs/gif1.gif)
--- app/src/main/java/com/biglist/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/biglist/ui/screens/HomeScreen.kt
@@ -2,9 +2,11 @@ package com.biglist.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -30,7 +32,7 @@ import com.biglist.ui.viewModels.HomeViewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.getValue
 import com.biglist.model.Post
-
+import androidx.compose.material3.CircularProgressIndicator
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel,
@@ -63,10 +65,14 @@ fun HomeScreen(
     }
 }
 
-
 @Composable
 fun LoadingIndicator() {
-    Text("Loading...")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
 }
 
 @Composable
